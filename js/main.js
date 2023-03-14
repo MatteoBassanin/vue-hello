35d609b0e7eba8bcdf495b31195470b82b9691b4
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-14 16:12:04 +0100

    img in dom

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        magica: 'Daje Roma!'
+        magica: 'Daje Roma!',
+        immagine: "https://clipground.com/images/as-roma-logo-14.jpg"
       }
     }
   }).mount('#app')
